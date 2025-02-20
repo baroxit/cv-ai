81a66e4779014cfabf12d325ae344e5d4ac33b31
barox <jack11.bari@gmail.com>
2025-02-20 16:53:39 +0100

    refactor: remove loading state reset in LoginForm and update login link in SignupForm

--- components/modules/auth/login-form.tsx
+++ components/modules/auth/login-form.tsx
@@ -35,7 +35,6 @@ export function LoginForm({
         variant: "destructive"
       })
     } finally {
-      setLoading(false);
       router.push('/dashboard/experiences')
     }
   };
--- components/modules/auth/signup-form.tsx
+++ components/modules/auth/signup-form.tsx
@@ -56,7 +56,7 @@ export function SignupForm({
             <h1 className="text-xl font-bold">Welcome to Acme Inc.</h1>
             <div className="text-center text-sm">
               Already have an account?{" "}
-              <Link href={'/auth/login'} className="underline underline-offset-4">
+              <Link href={'/login'} className="underline underline-offset-4">
                 Login
               </Link>
             </div>
