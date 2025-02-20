012d23e6eb9fb195e928d6f645e6dd0de371ae6e
barox <jack11.bari@gmail.com>
2025-02-20 18:11:12 +0100

    feat: add Vercel Analytics for improved tracking and insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono, Inter } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider"
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
+import { Analytics } from "@vercel/analytics/react"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -45,6 +46,7 @@ export default function RootLayout({
               {children}
           </ThemeProvider>
           <Toaster />
+          <Analytics />
       </body>
     </html>
   );
