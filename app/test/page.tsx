98c98a17b9a1433f3508d6e647b342a2aacd4319
barox <jack11.bari@gmail.com>
2025-03-15 19:53:14 +0100

    feat: implement CV editor page with user data fetching, mobile warning, and sidebar navigation

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,32 +1,102 @@
 "use client";
 
-import React, { useEffect, useState } from 'react';
-import { PDFViewer } from '@react-pdf/renderer';
-import { MyDocument } from '@/components/modules/pdf/document'; 
-import { useRouter } from 'next/navigation';
+import A4PageSkeleton from "@/components/a4page-skeleton";
+import {SidebarInset, SidebarTrigger} from "@/components/ui/sidebar";
+import {Separator} from "@/components/ui/separator";
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList, BreadcrumbPage,
+    BreadcrumbSeparator
+} from "@/components/ui/breadcrumb";
+import A4page from "@/components/a4page";
+import { getCv, updateCv } from "@/api/cv/serverActions";
+import { getUserData } from "@/api/about/serverActions";
 
-const App = () => {
-  const router = useRouter();
-  const [isClient, setIsClient] = useState(false);
+import CvPageContent from "@/components/modules/cv/cv-page-content";
+import { useParams } from 'next/navigation';
+import { useEffect, useState } from "react";
+import { userDataSchema } from "@/utils/schemas";
+import { CvSavingButton } from "@/components/modules/cv/saving-button";
+import { useRouter } from "next/navigation";
+import CvControls from "@/components/modules/cv/controls";
+import { Laptop2, Plus, Link, TreePalm, Laptop } from "lucide-react";
+import { useIsMobile } from "@/hooks/use-mobile";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
+export default function Page() {
+    const { id } = useParams();
+    const isMobile = useIsMobile();
+    const router = useRouter();
 
-  const handleClick = () => {
-    router.push(`dashboard/cv/6`);
-  }
+    const [userData, setUserData] = useState<userDataSchema>();  
+    const [cv, setCv] = useState<any>();
+    const [saving, setSaving] = useState<boolean>(false);
 
-  return (
-    <>
-      <button onClick={handleClick}>Router</button>
-      {isClient && (
-        <PDFViewer className='w-[1000px] h-[800px]'>
-          <MyDocument />
-        </PDFViewer>
-      )}
-    </>
-  );
+    async function fetchData() {
+        const userData = await getUserData();
+        const cv = await getCv(id as string);
+        setUserData(userData);
+        setCv(cv);
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, [id]);
+
+    if (isMobile) {
+        return (
+            <Card className="px-3 max-w-md py-4 text-center mx-auto mt-14 h-fit">
+                <CardHeader className="space-y-3"> 
+                    <div className="border p-3 mb-1.5 rounded-xl w-fit mx-auto">
+                        <Laptop2 size={32} strokeWidth={1.3} className="opacity-70" />
+                    </div>
+                    <CardTitle>Desktop Only Feature</CardTitle>
+                    <CardDescription className="text-balance pb-2">The CV editor is currently only available on desktop devices for the best editing experience. Please access this feature from a desktop computer.</CardDescription>
+                </CardHeader>
+            </Card>
+        );
+    }
+
+    if (userData && cv) {
+         return <SidebarInset>
+                <header className="flex h-16 shrink-0 items-center gap-2">
+                    <div className="flex items-center gap-2 px-4 w-full justify-between">
+                        <div className="flex items-center gap-2">
+                            <SidebarTrigger className="-ml-1" />
+                            <Separator orientation="vertical" className="mr-2 h-4" />
+                            <Breadcrumb>
+                                <BreadcrumbList>
+                                    <BreadcrumbItem className="hidden md:block">
+                                        <BreadcrumbLink href="#">
+                                            CVs
+                                        </BreadcrumbLink>
+                                    </BreadcrumbItem>
+                                    <BreadcrumbSeparator className="hidden md:block" />
+                                    <BreadcrumbItem>
+                                        <BreadcrumbPage>{cv.title}</BreadcrumbPage>
+                                    </BreadcrumbItem>
+                                </BreadcrumbList>
+                            </Breadcrumb>
+                        </div>
+                        <CvControls />
+                    </div>
+                </header>
+
+                <div className="flex flex-1 flex-col gap-4 pt-0">
+                    <CvSavingButton savedStatus={!saving} className={"fixed bottom-3 z-10 ml-3"} />
+                    <A4page>
+                        <CvPageContent
+                            userData={userData}
+                            cv={cv}
+                            onChangeSaving={setSaving}
+                        />                        
+                    </A4page>
+                </div>
+            </SidebarInset>
+    } else {
+        return (<A4PageSkeleton />)
+    }
 };
-
-export default App;
