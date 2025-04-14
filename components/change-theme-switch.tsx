1a4606970c2a356c26a474b0b8e09960f327bd8a
matteozamu <matteoz2001@gmail.com>
2025-04-14 13:26:42 +0200

    initialise theme state on dark theme switch to add reactivness

--- components/change-theme-switch.tsx
+++ components/change-theme-switch.tsx
@@ -1,26 +1,30 @@
-"use client"
+'use client'
 
-import { Label } from "@/components/ui/label";
-import { Switch } from "@/components/ui/switch";
-import { useState } from "react";
-import { useTheme } from "next-themes";
+import { Label } from '@/components/ui/label'
+import { Switch } from '@/components/ui/switch'
+import { useState, useEffect } from 'react'
+import { useTheme } from 'next-themes'
 
 export function ChangeThemeSwitch() {
-    const { theme, setTheme } = useTheme();
-    const [darkTheme, setDarkTheme] = useState<boolean>(true);
+	const { theme, setTheme } = useTheme()
+	const [darkTheme, setDarkTheme] = useState<boolean>(true)
 
-    const changeTheme = (checked: boolean) => {
-        setDarkTheme(checked);
-        setTheme(checked ? "dark" : "light");
-    };
+	// Sync the local state with the theme when it's available
+	useEffect(() => {
+		if (theme) {
+			setDarkTheme(theme === 'dark')
+		}
+	}, [theme])
 
-    return (
-        <div className="flex flex-row items-center justify-between rounded-lg border p-3 mt-1.5 shadow-sm">
-            <Label>Dark mode</Label>
-            <Switch
-                checked={darkTheme}
-                onCheckedChange={(checked) => changeTheme(checked)}
-            />
-        </div>
-    );
+	const changeTheme = (checked: boolean) => {
+		setDarkTheme(checked)
+		setTheme(checked ? 'dark' : 'light')
+	}
+
+	return (
+		<div className='flex flex-row items-center justify-between rounded-lg border p-3 mt-1.5 shadow-sm'>
+			<Label>Dark mode</Label>
+			<Switch checked={darkTheme} onCheckedChange={(checked) => changeTheme(checked)} />
+		</div>
+	)
 }
